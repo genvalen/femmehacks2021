dec6b935e6ab0813a3e1507feb97b9f0c0e5c45e
anitayang <asuryani2151@gmail.com>
2021-02-07 08:07:14 +1300

    send sms

--- send_sms.py
+++ send_sms.py
@@ -2,20 +2,43 @@
 # Download the helper library from https://www.twilio.com/docs/python/install
 import os
 from twilio.rest import Client
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
 
+app = Flask(__name__)
+
+@app.route("/sms", methods=['GET', 'POST'])
+def incoming_sms():
+    """Send a dynamic reply to an incoming text message"""
+    # Get the message the user sent our Twilio number
+    body = request.values.get('Body', None)
+
+    # Start our TwiML response
+    resp = MessagingResponse()
+
+    # Determine the right reply for this message
+    if body == 'hello':
+        resp.message("Hi!")
+    elif body == 'bye':
+        resp.message("Goodbye")
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 # Your Account Sid and Auth Token from twilio.com/console
 # and set the environment variables. See http://twil.io/secure
 account_sid = 'AC4794f18e08a357df60339cfaa88bca58'
-auth_token = 'a15ad087ff31013ed093e90bc29d05ba'
+auth_token = 'xxx'
 client = Client(account_sid, auth_token)
 
 message = client.messages \
-                .create(
-                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-                     messaging_service_sid = 'MG5250661f2ab8628e6520b33f905daf4f',
-                     to='+64272130734'
-                 )
+                    .create(
+                         body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                         messaging_service_sid = 'MG5250661f2ab8628e6520b33f905daf4f',
+                         to='+64272130734'
+                     )
 
 print(message.sid)
 
