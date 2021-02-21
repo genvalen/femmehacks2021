b87d9ca1ed5f71f3f104e313f97545f92a343595
genvalen <genvalen@protonmail.com>
2021-02-21 17:52:43 -0500

    Pickle and unpickle df, then move df under if name main block

--- recommender.py
+++ recommender.py
@@ -4,6 +4,7 @@ from flask import Flask, request, redirect, send_file
 import requests
 from twilio.twiml.messaging_response import MessagingResponse
 
+import pickle
 import json
 import numpy as np
 import pandas as pd
@@ -34,13 +35,11 @@ def bot():
         msg.body("Hi there! Which category do you want to donate to:'Environment', 'Arts, Culture, Humanities', 'Religion','Human Services', 'Education', 'Animals', 'International','Health', 'Community Development', 'Human and Civil Rights','Research and Public Policy'?")
     return str(resp)
 
-charities_df = pd.read_csv(filename, header=0, sep=',',index_col=False, encoding='utf8',lineterminator='\n')
 
 def get_org_recs(category, size):
     # memory = json.loads(request.form.get('Memory'))
-    filename = 'CLEAN_charity_data.csv'
-
-    df_cat = charities_df.loc[df['category'] == category]
+    
+    df_cat = df.loc[df['category'] == category]
     df_size = df_cat.loc[df['size'] == size]
     df_sorted = df_size.sort_values('score', ascending=False)
     a = df_sorted.iloc[0]['name'] + ", "
@@ -51,4 +50,13 @@ def get_org_recs(category, size):
 
 
 if __name__ == "__main__":
+    # make csv into dataframe, pickle dataframe, then unpickle
+    # this section will be updated when integrated with gcp storage
+    input_file = 'CLEAN_charity_data.csv'
+    pickled_file = 'charities_df.pickle'
+    charities_df = pd.read_csv(input_file, header=0, sep=',',index_col=False, encoding='utf8',lineterminator='\n')
+    charities_df.to_pickle(pickled_file)
+    df = pd.read_pickle(pickled_file)
+
+
     app.run(debug=True)
